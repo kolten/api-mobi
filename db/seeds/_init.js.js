6c54653bd27c0c0c9385e45339e9fd75134c33fa
Kolten Sturgill <koltensturgill92@gmail.com>
2019-01-14 13:38:53 -0600

    uncommented seeds

--- db/seeds/_init.js.js
+++ db/seeds/_init.js.js
@@ -4,9 +4,9 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
   .then(function() {
     return knex('users').insert([
-      {first_name: 'Kolten', last_name: 'Sturgill', student_id: '1001089599', email: 'kolten.sturgill@mavs.uta.edu', password: hat(), is_admin: true, member: true, email_verified: false, shirt_size: 'M'},
-      // {id: 2, first_name: 'Mary', last_name: 'Huerta', student_id: '1001000000', email: 'mary.huerta@mavs.uta.edu', password: hat(), shirt_size: 'S'},
-      // {id: 3, first_name: 'Zach', last_name: 'Gentry', student_id: '1001011111', email: 'zach.gentry@mavs.uta.edu', password: hat(), shirt_size: 'M'},
+      { first_name: 'Kolten', last_name: 'Sturgill', student_id: '1001089599', email: 'kolten.sturgill@mavs.uta.edu', password: hat(), is_admin: true, member: true, email_verified: false, shirt_size: 'M'},
+      { first_name: 'Mary', last_name: 'Huerta', student_id: '1001000000', email: 'mary.huerta@mavs.uta.edu', password: hat(), shirt_size: 'S'},
+      { first_name: 'Zach', last_name: 'Gentry', student_id: '1001011111', email: 'zach.gentry@mavs.uta.edu', password: hat(), shirt_size: 'M'},
     ]);
   })
   .then(function(){
@@ -15,8 +15,8 @@ exports.seed = function(knex, Promise) {
   .then(function() {
     return knex('password_resets').insert([
       {user_id: 1, token: hat()},
-      // {user_id: 2, token: hat()},
-      // {user_id: 3, token: hat()}
+      {user_id: 2, token: hat()},
+      {user_id: 3, token: hat()}
     ]);
   })
   .then(function () {
